539f35dbadb7349bc3b9df6c7e674b3792ad1a6e
alpaca-tc <alpaca-tc@alpaca.tc>
2021-11-29 16:45:36 +0900

    Install eslint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import type { TextlintPluginCreator } from '@textlint/types';
+import type { TextlintPluginCreator } from "@textlint/types";
 import type {
   TextlintPluginOptions,
   TextlintPluginProcessor,
-} from '@textlint/types';
+} from "@textlint/types";
 import { rubyToAST } from "./rubyToAST";
 
 class Processor implements TextlintPluginProcessor {
@@ -10,26 +10,26 @@ class Processor implements TextlintPluginProcessor {
   private extensions: string[];
 
   constructor(options?: TextlintPluginOptions) {
-    this.execCommand = options?.execCommand ?? ['textlint-ruby']
+    this.execCommand = options?.execCommand ?? ["textlint-ruby"];
     this.extensions = options?.extensions ?? [];
   }
 
   availableExtensions() {
-    return [".rb", ...this.extensions]
+    return [".rb", ...this.extensions];
   }
 
   public processor() {
-    const self = this
+    const execCommand = this.execCommand;
 
     return {
       preProcess(text: string, filePath?: string) {
-        return rubyToAST(self.execCommand, text, filePath);
+        return rubyToAST(execCommand, text, filePath);
       },
 
       postProcess(messages: any[], filePath?: string) {
         return {
           messages,
-          filePath: filePath ?? '<ruby>',
+          filePath: filePath ?? "<ruby>",
         };
       },
     };
--- src/rubyToAST.ts
+++ src/rubyToAST.ts
@@ -1,51 +1,54 @@
-import type {
-  AnyTxtNode
-} from '@textlint/ast-node-types';
+import type { AnyTxtNode } from "@textlint/ast-node-types";
 
-import { tmpNameSync } from 'tmp'
-import { writeFileSync } from "fs"
-import { spawnSync } from "child_process"
+import { tmpNameSync } from "tmp";
+import { writeFileSync } from "fs";
+import { spawnSync } from "child_process";
 
 const die = (error: Error): void => {
-  console.error(error)
-  process.exit(1)
-}
+  console.error(error);
+  process.exit(1);
+};
 
-const execTextlintRuby = (execCommand: string[], text: string, filePath: string | undefined): any => {
+const execTextlintRuby = (
+  execCommand: string[],
+  text: string,
+  filePath: string | undefined
+): any => {
   let path: string;
 
   if (filePath) {
-    path = filePath
+    path = filePath;
   } else {
-    path = tmpNameSync({ postfix: '.rb' }).toString();
-    writeFileSync(path, text)
+    path = tmpNameSync({ postfix: ".rb" }).toString();
+    writeFileSync(path, text);
   }
 
   try {
-    const commandArgs = [...execCommand, path]
-    const cmd = commandArgs.shift()
+    const commandArgs = [...execCommand, path];
+    const cmd = commandArgs.shift();
 
     // Parse ruby source code with textlint-ruby
-    const spawn = spawnSync(
-      cmd!,
-      commandArgs
-    )
+    const spawn = spawnSync(cmd!, commandArgs);
 
     if (spawn.error) {
-      die(spawn.error!)
+      die(spawn.error!);
     }
 
-    const json = spawn.stdout.toString()
-    return JSON.parse(json) as AnyTxtNode
-  } catch(error) {
+    const json = spawn.stdout.toString();
+    return JSON.parse(json) as AnyTxtNode;
+  } catch (error) {
     if (error instanceof Error) {
-      die(error)
+      die(error);
     } else {
-      die(new Error(String(error)))
+      die(new Error(String(error)));
     }
   }
-}
+};
 
-export const rubyToAST = (execCommand: string[], text: string, filePath: string | undefined): AnyTxtNode => {
-  return execTextlintRuby(execCommand, text, filePath) as AnyTxtNode
-}
+export const rubyToAST = (
+  execCommand: string[],
+  text: string,
+  filePath: string | undefined
+): AnyTxtNode => {
+  return execTextlintRuby(execCommand, text, filePath) as AnyTxtNode;
+};
